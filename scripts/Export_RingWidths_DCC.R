964cd4c90cf3da1e2724756241e1698020657df1
Sierra <mac.mouse98@gmail.com>
2017-07-27 18:56:24 -0500

    Script for formatting rwl files and the begining of cross dating
    Set up the script to reformat tree ring files and export for use with cofecha. Began the cross dating process with all of the oaks as well as two smaller subsets (happy and happy2). "Happy" is 32 trees with correlations above .55, "Happy2" is 9 trees with correlations above .7

--- scripts/Export_RingWidths_DCC.R
+++ scripts/Export_RingWidths_DCC.R
@@ -1,170 +1,74 @@
-# Combine files for DCC partial ring widths
-# CHG w/ RLB 12-14-16
+# --------------------------------------
+# Organizing and reformatting tree ring data
+# Sierra Lopezalles, slopezal@caltech.edu
+# July 27, 2017
+# --------------------------------------
+# --------------------------------------
 
 library(dplR)
-#setwd('~/Navajo Dendroclimate/Sites/CrossCanyon/')
 
-files <- Sys.glob('West_peak/EW_LW/*.rwl')
+# Setting a working directory
 
-# pulling remeasured files
-files.redo <- Sys.glob('West_peak/EW_LW/redo/*.rwl')
+setwd("~/Github/URF2017/")
 
-# Total ring widths -------------------------------------------------------
+# Pulling all raw tree ring files into one object
+# CHANGE HERE IF USING ANOTHER FOLDER
 
-trw.files <- files[grep('ring width', files)]
+rawringfiles <- Sys.glob('~/Google Drive/Morton Summer 2017/East Woods/Rollinson_Monitoring/Data/Tree Cores/RawRingWidths/*.rwl')
 
-trw.obj <- NA
-for(i in trw.files) {
-  aa <- read.rwl(i)
-  nam <- paste(names(aa), '_t', sep='')
-  trw.obj <- append(trw.obj, nam)
-  assign(nam, aa)
+# Grabbing all the species data
+
+permtree.data <-read.csv(file = "~/Github/EastWoods-MonitoringPlots/TreeCensus_2017/data/TreeData-raw_data.csv", na.strings = "", colClasses=c("Tag"="character"))
+temptree.data <-read.csv("~/Github/EastWoods-MonitoringPlots/TreeCensus_2017/data/URF_2017_AdditionalOakData-raw_data.csv", na.strings="", colClasses=c("Tag"="character"))
+
+names(permtree.data)[6] <- "Species"
+permtree.data[,3] <- as.character(permtree.data[,3])
+temptree.data[,3] <- as.character(temptree.data[,3])
+
+tree.species <- rbind(permtree.data[,c(3,6)], temptree.data[,c(3,4)])                     
+tree.species$Species <- as.factor(substr(tree.species$Species, 1, 2))
+
+# Setting a path to the raw ring width folder
+
+tr.path <- "~/Google Drive/Morton Summer 2017/East Woods/Rollinson_Monitoring/Data/Tree Cores/RawRingWidths/"
+
+# Splitting the file name into lists
+
+rw.names <- dir(tr.path)
+trw.names <- rw.names[grep('ring width', rw.names)]
+trw.names.split <- strsplit(trw.names, "-")
+
+# --------------------------------------
+# Combining and renaming ring width files
+# --------------------------------------
+
+# Initializing the master list with the first ring width set
+
+# Reading in the first file
+files.all <- read.rwl(file.path(tr.path, rw.names[1])) 
+
+# Storing the parts of the name
+aa <- trw.names.split[[1]]  
+
+# Combining the parts of the name with species in the proper order
+nam <- paste0(as.character(tree.species[tree.species$Tag==aa[3],2]), paste(aa[c(3, 4, 6)], collapse=""))
+
+# Assigning the name to the file
+names(files.all) <- nam
+
+# Repeating for all other files 
+
+for(i in 2:length(trw.names)){
+  file.tmp <- read.rwl(file.path(tr.path, trw.names[i]))
+  aa <- trw.names.split[[i]]
+  nam <- paste0(as.character(tree.species[tree.species$Tag==aa[3],2]), paste(aa[c(3, 4, 6)], collapse=""))
+  names(file.tmp) <- nam
+  
+  # Appending each file to the master list
+  files.all <- combine.rwl(files.all, file.tmp)
 }
 
-trw.obj <- na.omit(trw.obj)
+# CHECK NAME
 
-all.trw <- combine.rwl(get(trw.obj[1]), get(trw.obj[2]))
+write.rwl(files.all, "oaks_master_trw.rwl", long.names=TRUE)
 
-for(i in 3:length(trw.obj)){
-  aa <- get(trw.obj[i])
-  all.trw <- combine.rwl(all.trw, aa)
-}
-
-write.tucson(all.trw, "west_peak_trw.rwl", prec=0.001, long.names=TRUE)
-
-
-# Pulling remeasured files
-trw.files <- files.redo[grep('ring width', files.redo)]
-
-trw.obj <- NA
-for(i in trw.files) {
-  aa <- read.rwl(i)
-  nam <- paste(names(aa), '_t', sep='')
-  trw.obj <- append(trw.obj, nam)
-  assign(nam, aa)
-}
-
-trw.obj <- na.omit(trw.obj)
-
-redo.trw <- combine.rwl(get(trw.obj[1]), get(trw.obj[2]))
-
-for(i in 3:length(trw.obj)){
-  aa <- get(trw.obj[i])
-   redo.trw <- combine.rwl(redo.trw, aa)
-}
-
-write.tucson(redo.trw, "west_peak_redo_trw.rwl", prec=0.001, long.names=TRUE)
-
-
-
-summary(all.trw)
-# Earlwood widths ---------------------------------------------------------
-
-erw.files <- files[grep('earlywood width', files)]
-
-erw.obj <- NA
-for(i in erw.files) {
-  aa <- read.rwl(i)
-  nam <- paste(names(aa), '_e', sep='')
-  erw.obj <- append(erw.obj, nam)
-  assign(nam, aa)
-}
-
-erw.obj <- na.omit(erw.obj)
-
-all.erw <- combine.rwl(get(erw.obj[1]), get(erw.obj[2]))
-
-for(i in 3:length(erw.obj)){
-  aa <- get(erw.obj[i])
-  all.erw <- combine.rwl(all.erw, aa)
-}
-
-write.tucson(all.erw, "west_peak_ew.rwl", prec=0.001, long.names=TRUE)
-
-# Redo EW
-erw.redo <- files.redo[grep('earlywood width', files.redo)]
-
-erw.obj <- NA
-for(i in erw.redo) {
-  aa <- read.rwl(i)
-  nam <- paste(names(aa), '_e', sep='')
-  erw.obj <- append(erw.obj, nam)
-  assign(nam, aa)
-}
-
-erw.obj <- na.omit(erw.obj)
-
-redo.erw <- combine.rwl(get(erw.obj[1]), get(erw.obj[2]))
-
-for(i in 3:length(erw.obj)){
-  aa <- get(erw.obj[i])
-  redo.erw <- combine.rwl(redo.erw, aa)
-}
-
-write.tucson(redo.erw, "west_peak_ew_redo.rwl", prec=0.001, long.names=TRUE)
-
-
-# Latewood widths ---------------------------------------------------------
-
-lrw.files <- files[grep('latewood width', files)]
-
-lrw.obj <- NA
-for(i in lrw.files) {
-  aa <- read.rwl(i)
-  nam <- paste(names(aa), '_l', sep='')
-  lrw.obj <- append(lrw.obj, nam)
-  assign(nam, aa)
-}
-
-lrw.obj <- na.omit(lrw.obj)
-
-all.lrw <- combine.rwl(get(lrw.obj[1]), get(lrw.obj[2]))
-
-for(i in 3:length(lrw.obj)){
-  aa <- get(lrw.obj[i])
-  all.lrw <- combine.rwl(all.lrw, aa)
-}
-
-write.tucson(all.lrw, "west_peak_lww.rwl", prec=0.001, long.names=TRUE)
-
-
-# LW redo
-
-lrw.redo <- files.redo[grep('latewood width', files.redo)]
-
-lrw.obj <- NA
-for(i in lrw.redo) {
-  aa <- read.rwl(i)
-  nam <- paste(names(aa), '_l', sep='')
-  lrw.obj <- append(lrw.obj, nam)
-  assign(nam, aa)
-}
-
-lrw.obj <- na.omit(lrw.obj)
-
-redo.lrw <- combine.rwl(get(lrw.obj[1]), get(lrw.obj[2]))
-
-for(i in 3:length(lrw.obj)){
-  aa <- get(lrw.obj[i])
-  redo.lrw <- combine.rwl(redo.lrw, aa)
-}
-
-write.tucson(redo.lrw, "west_peak_lww_redo.rwl", prec=0.001, long.names=TRUE)
-
-
-# Chronology stats --------------------------------------------------------
-
-
-
-west.peak.det <- detrend(all.trw, method="Spline")
-#ocw.ids <- read.ids(ocw.det, stc=c(3, ))
-rwi.stats(west.peak.det)
-
-
-ocw.chron <- chron(west.peak.det, pref='PWP')
-
-plot(ocw.chron)
-
-seg.plot(west.peak.det)
-
-test <- corr.rwl.seg(all.trw)
