5af182130296de051c70ef63a81c50c5cb133a59
Christy Rollinson <crollinson@gmail.com>
2017-08-02 13:07:58 -0500

    Add preliminary 2017 burn info

--- scripts/extract_burn_info.R
+++ scripts/extract_burn_info.R
@@ -28,6 +28,7 @@ path.gis <- "/Volumes/GIS/"
 # --------------------------------------
 # Load GIS layers
 burn <- readOGR(file.path(path.gis, "Collections/Natural Resources Management/Burn", "Burned_Area.shp"))
+burn2017 <- readOGR(file.path(path.gis, "Collections/Natural Resources Management/Burn", "Spring_2017_Burned_Area.shp"))
 summary(burn)
 
 # Get table of plot locations
@@ -44,8 +45,17 @@ summary(plots.sp)
 # --------------------------------------
 # Extracting the burn info for each point
 # --------------------------------------
+# Doing a bit of cleaning to the 2017 burn file
+burn2017 <- burn2017[,c("OBJECTID", "Burn_Date", "Location", "Acres", "Notes")]
+names(burn2017)[c(1,5)] <- c("Id", "NOTES")
+burn2017$Burn_Date <- as.factor(2017)
+summary(burn2017)
+
 # Extracting the burn info
 plot.burn <- extract(burn, plots.sp)
+plot.burn2017 <- extract(burn2017, plots.sp)
+
+plot.burn <- merge(plot.burn, plot.burn2017, all=T)
 summary(plot.burn)
 
 # Adding an ID and adding some of our plot info to the burn report
