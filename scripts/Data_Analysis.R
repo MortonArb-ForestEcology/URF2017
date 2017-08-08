4a3455de5a6471c3addb0497907f0630c3627504
crollinson <crollinson@gmail.com>
2017-08-07 22:06:19 -0700

    Adding an example of a gam
    The gam lets us build in some of the detrending that normally happens
    behind the scenes in tree ring analyses

--- scripts/Data_Analysis.R
+++ scripts/Data_Analysis.R
@@ -10,12 +10,12 @@
 # --------------------------------------
 
 # Working directory path
-path.wd <- "~/Github/URF2017/" # Sierra
-# path.wd <- "~/Desktop/Research/URF2017_Lopazelles/" # Christy
+# path.wd <- "~/Github/URF2017/" # Sierra
+path.wd <- "~/Desktop/Research/URF2017_Lopazelles/" # Christy
 
 # Path to East Woods Google Drive folder
 path.google <- "C:/Users/macmo/Google Drive/Morton Summer 2017/East Woods/" # Sierra
-# path.google <- "~/Google Drive/East Woods" # Christy
+# path.google <- "~/Google Drive/East Woods/" # Christy
 
 # --------------------------------------
 # Load libraries, define working directory
@@ -54,6 +54,7 @@ trw.data <- trw.data[trw.data$Year >= 2000,]
 ggplot(trw.data) +
   geom_point(aes(x=FireCount, y=RingWidth)) +
   geom_smooth(aes(x=FireCount, y=RingWidth), method="lm")
+
 
 lm.test1 <- lm(RingWidth ~ FireCount, data=trw.data)
 summary(lm.test1)
@@ -99,6 +100,18 @@ summary(lm.test9)
 
 lm.test10 <- lm(BAI ~ Precip+Temp+FireCount+Year+Species, data=trw.data)
 summary(lm.test10)
+
+# An example of how to make a better model by building detrending into the model
+# Plot will need to be changed to whatever the name of the column is that tells you 
+# what tree things came from; if by=Plot runs quickly, try changing it to Tag 
+# (or whatever indicates the unique tree)
+gam1 <- gam(BAI ~ (Precip*Temp + FireCount)*Species + s(Year, by=Plot), data=trw.data)
+summary(gam1)
+plot(gam1) # hit escape to stop graphing, but think about what this is actually showing
+
+# GAMM example (with random effects!); replace Plot, Tree & Core with whatever the appropraite column names are
+library(mgcv)
+gamm.example <- gamm(BAI ~ (Precip*Temp + FireCount)*Species + s(Year, by=Plot), random=list(Tree=~1, Core=~1), data=trw.data)
 
 hist(trw.data$BAI); abline(v=median(trw.data$BAI), lty="dashed", col="red", lwd=5)
 hist(resid(lm.test10)); abline(v=0, lty="dashed", col="red", lwd=5)
