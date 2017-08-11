cd955c145fba8ba95de00f5ec72004c7cfe2af04
Sierra <mac.mouse98@gmail.com>
2017-08-11 11:41:25 -0500

    Created more models and graphs
    Poorly annotated at the moment

--- scripts/3_DataAnalysis.R
+++ scripts/3_DataAnalysis.R
@@ -37,6 +37,13 @@ trw.data <- read.csv(file.path(path.wd,"data/CombinedData"))
 # Removing weird first column
 trw.data <- trw.data[,2:length(trw.data)]
 
+# Categorical for boxplots
+
+trw.data.cat <- trw.data
+trw.data.cat$BurnIn3 <- as.factor(trw.data$BurnIn3)
+trw.data.cat$SinceBurn <- as.factor(trw.data$SinceBurn)
+trw.data.cat$FireCount <- as.factor(trw.data$FireCount)
+
 # --------------------------------------
 # Analysis and graphs
 # --------------------------------------
@@ -56,6 +63,11 @@ ggplot(trw.data) +
   geom_point(aes(x=FireCount, y=BAI, color = Species)) +
   geom_smooth(aes(x=FireCount, y=BAI, color = Species), method="lm")
 
+ggplot() +
+  geom_jitter(aes(x=FireCount, y=BAI, color = Species), alpha=.3, data=trw.data.cat) +
+  geom_boxplot(aes(x=FireCount, y=BAI, color = Species), data=trw.data.cat) 
+  #geom_smooth(aes(x=FireCount, y=BAI), method="lm", data=trw.data)
+
 # R2 = .3837
 lm.test1 <- lm(BAI ~ (Precip*Temp + FireCount)*Species + Year + Plot, data=trw.data)
 summary(lm.test1)
@@ -67,11 +79,11 @@ plot(gam1)
 
 # Seperating by species
 
-# R2 = .3599
+# R2 = .3599 ***
 lm.test1QUMA <- lm(BAI ~ Precip*Temp + FireCount + Year + Plot, data=trw.data[trw.data$Species == "QUMA",])
 summary(lm.test1QUMA)
 
-# R2 = .348
+# R2 = .348 ***
 gam1QUMA <- gam(BAI ~ Precip*Temp + FireCount + s(Year, by=Tag), data=trw.data[trw.data$Species == "QUMA",])
 summary(gam1QUMA)
 plot(gam1)
@@ -81,7 +93,7 @@ plot(gam1)
 # --------------------------------------
 
 ggplot(trw.data) +
-  geom_jitter(aes(x=BurnIn3, y=BAI, color = Species)) +
+  geom_point(aes(x=BurnIn3, y=BAI, color=Species)) +
   geom_smooth(aes(x=BurnIn3, y=BAI, color = Species), method="lm")
 
 # R2 = .294
@@ -105,21 +117,26 @@ summary(gam3QUMA)
 ggplot(trw.data[trw.data$Species == "QUMA",]) +
   geom_point(aes(x=BurnIn3, y=BAI, color = Species)) +
   geom_smooth(aes(x=BurnIn3, y=BAI, color = Species), method="lm")
+
 # --------------------------------------
 # Years since last burn, with 10 as max
 # --------------------------------------
 
 ggplot(trw.data) +
-  geom_jitter(aes(x=SinceBurn, y=BAI, color = Species)) +
-  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+  geom_jitter(aes(x=SinceBurn, y=BAI, color=Species)) +
+  geom_smooth(aes(x=SinceBurn, y=BAI, color=Species), method="lm")
 
 # R2 = .3
 gam2 <- gam(BAI ~ (Precip*Temp + SinceBurn)*Species + s(Year, by=Tag), data=trw.data)
 summary(gam2)
 
-# R2 = .374
-lm.test2 <- lm(BAI ~ (Precip*Temp + SinceBurn)*Species + Year + Plot, data=trw.data)
+# R2 = .374 *
+lm.test2 <- lm(BAI ~ (Precip*Temp + SinceBurn)*Species + Plot + Year, data=trw.data)
 summary(lm.test2)
+
+# R2 = .4735 
+lm.test22 <- lm(BAI ~ (Precip*Temp + SinceBurn)*Species*Plot + Year, data=trw.data)
+summary(lm.test22)
 
 # --------------------------------------
 # Seperating by species
@@ -135,8 +152,8 @@ gam2QUMA <- gam(BAI ~ Precip*Temp + SinceBurn + s(Year, by=Plot), data=trw.data[
 summary(gam2QUMA)
 
 ggplot(trw.data[trw.data$Species == "QUMA",]) +
-  geom_point(aes(x=SinceBurn, y=BAI, color = Species)) +
-  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+  geom_point(aes(x=SinceBurn, y=BAI, color = Plot)) +
+  geom_smooth(aes(x=SinceBurn, y=BAI, color = Plot), method="lm")
 
 # QUAL ---------------------------------
 
@@ -148,9 +165,8 @@ summary(lm.test2QUAL)
 gam2QUAL <- gam(BAI ~ Precip*Temp + SinceBurn + s(Year, by=Tag), data=trw.data[trw.data$Species == "QUAL",])
 summary(gam2QUAL)
 
-ggplot(trw.data[trw.data$Species == "QUAL",]) +
-  geom_point(aes(x=SinceBurn, y=BAI, color = Species)) +
-  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+ggplot(trw.data.cat[trw.data$Species == "QUAL",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI, color = Species)) 
 
 # QURU ---------------------------------
 
@@ -163,8 +179,8 @@ gam2QURU <- gam(BAI ~ Precip*Temp + SinceBurn + s(Year, by=Tag), data=trw.data[t
 summary(gam2QURU)
 
 ggplot(trw.data[trw.data$Species == "QURU",]) +
-  geom_point(aes(x=SinceBurn, y=BAI, color = Species)) +
-  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+  geom_point(aes(x=SinceBurn, y=BAI, color = Plot)) +
+  geom_smooth(aes(x=SinceBurn, y=BAI, color = Plot), method="lm")
 
 # ACSA ---------------------------------
 
@@ -177,8 +193,8 @@ gam2ACSA <- gam(BAI ~ Precip*Temp + SinceBurn + s(Year, by=Tag), data=trw.data[t
 summary(gam2ACSA)
 
 ggplot(trw.data[trw.data$Species == "ACSA",]) +
-  geom_point(aes(x=SinceBurn, y=BAI, color = Species)) +
-  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+  geom_point(aes(x=SinceBurn, y=BAI, color = Plot)) +
+  geom_smooth(aes(x=SinceBurn, y=BAI, color = Plot), method="lm")
 
 # TIAM ---------------------------------
 
@@ -191,8 +207,8 @@ gam2TIAM <- gam(BAI ~ Precip*Temp + SinceBurn + s(Year, by=Tag), data=trw.data[t
 summary(gam2TIAM)
 
 ggplot(trw.data[trw.data$Species == "TIAM",]) +
-  geom_point(aes(x=SinceBurn, y=BAI, color = Species)) +
-  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+  geom_point(aes(x=SinceBurn, y=BAI, color = Plot)) +
+  geom_smooth(aes(x=SinceBurn, y=BAI, color = Plot), method="lm")
 
 # --------------------------------------
 
@@ -207,5 +223,105 @@ gam2QU <- gam(BAI ~ Precip*Temp + SinceBurn + s(Year, by=Tag), data=trw.data[trw
 summary(gam2QU)
 
 ggplot(trw.data[trw.data$Genus == "QU",]) +
-  geom_point(aes(x=SinceBurn, y=BAI, color = Species)) +
+  geom_point(aes(x=SinceBurn, y=BAI)) +
   geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+
+
+# --------------------------------------
+# Unburned plot comparison
+# --------------------------------------
+
+trw.unburned <- trw.data[trw.data$SinceBurn == 10,]
+
+# R2 = .3937 
+lm.plot <- lm(BAI ~ (Precip*Temp)*Species*Plot + Year + Tag,
+              data=trw.unburned)
+summary(lm.plot)
+
+lm.a128 <- lm(BAI ~ Species + Tag,
+              data=trw.unburned[trw.unburned$Plot == "R109",])
+summary(lm.a128)
+
+ggplot(trw.data.cat) +
+  geom_boxplot(aes(x=Species, y=BAI, color=Plot))
+
+# --------------------------------------
+
+bai.diff <- trw.data[,c("Tag", "Core", "Species", "Plot", "Year")]
+
+
+for (i in 1:nrow(trw.data)){
+  if (trw.data[i, "LastBurn"] == trw.data[i, "Year"]){
+    now.bai.tmp <- trw.data[i, "BAI"]
+    before.bai.tmp <- trw.data[trw.data$Year == trw.data[i, "Year"] - 1 & trw.data$Core == trw.data[i, "Core"], "BAI"]
+    bai.diff[bai.diff$Core == trw.data[i, "Core"] & bai.diff$Year == trw.data[i, "Year"],"BurnYear"] <- now.bai.tmp
+    bai.diff[bai.diff$Core == trw.data[i, "Core"] & bai.diff$Year == trw.data[i, "Year"],"BeforeBurn"] <- before.bai.tmp
+  
+  }
+}
+
+bai.diff$Difference <- bai.diff$BurnYear - bai.diff$BeforeBurn
+
+bai.diff <- bai.diff[!is.na(bai.diff$Difference),]
+
+bai.order <- order(bai.diff$Tag)
+
+bai.diff <- bai.diff[bai.order,]
+
+bai.diff <- unique(bai.diff)
+
+bai.diff$Percent <- bai.diff$Difference / bai.diff$BeforeBurn
+
+mean(bai.diff[bai.diff$Species == "QUMA","Difference"])
+sd(bai.diff$Difference)
+
+mean(bai.diff$Percent)
+sd(bai.diff$Percent)
+
+# --------------------------------------
+
+trw.oneyear <- trw.data[trw.data$SinceBurn <= 1,]
+
+ggplot(trw.oneyear) +
+  geom_jitter(aes(x=SinceBurn, y=BAI, color = Species), width = .1) +
+  geom_smooth(aes(x=SinceBurn, y=BAI, color = Species), method="lm")
+
+# R2 = .3665 *
+
+lm.oneyear <- lm(BAI ~ (Precip+Temp)*Species + SinceBurn + Year + Plot + Tag,
+              data=trw.oneyear)
+summary(lm.oneyear)
+
+gam.oneyear <- gam(BAI ~ (Precip+Temp)*Species+ SinceBurn + s(Year, by=Plot), data=trw.oneyear)
+summary(gam.oneyear)
+
+# --------------------------------------
+
+ggplot(trw.data) +
+  geom_boxplot(aes(x=FireCount, y=BAI, color = Plot))
+
+ggplot(trw.data) +
+  geom_boxplot(aes(x=BurnIn3, y=BAI, color = Plot))
+
+ggplot(trw.data) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI))
+
+ggplot(trw.data[trw.data$Species == "QUMA",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI, color = Plot))
+
+ggplot(trw.data[trw.data$Species == "QUAL",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI, color = Plot)) 
+
+ggplot(trw.data[trw.data$Species == "QURU",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI, color = Plot))
+
+ggplot(trw.data[trw.data$Genus == "QU",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI))
+
+ggplot(trw.data[trw.data$Species == "ACSA",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI, color = Plot))
+
+ggplot(trw.data[trw.data$Species == "TIAM",]) +
+  geom_boxplot(aes(x=SinceBurn, y=BAI, color = Plot))
+
+
